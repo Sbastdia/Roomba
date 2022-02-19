4906bd283cbe4666a5fef7781f34519bc68b84e6
Sbastdia <91331243+Sbastdia@users.noreply.github.com>
2022-02-19 12:30:04 +0000

    Cambios iniciales

---
+++ main.py
@@ -0,0 +1,17 @@
+
+import tkinter as tk
+import alimpiar
+import lista
+import diccionarios
+import script
+import tupla
+window1 = tk.Tk()
+window1.title("Hora de hacer una Roomba")
+window1.geometry("300x300")
+
+hello = tk.Label(text="¿Cuantas zonas tiene tu casa?")
+hello.pack()
+button = tk.Button(text="Pulsa")
+button.pack()
+
+tk.mainloop()
