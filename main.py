272bf1c5d146612a79dae2a8f6af688f3fc37fce
Sbastdia <91331243+Sbastdia@users.noreply.github.com>
2022-02-19 12:34:09 +0000

    cambios iniciales

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import tupla
 window1 = tk.Tk()
 window1.title("Hora de hacer una Roomba")
 window1.geometry("300x300")
-
+zonas=tk.Spinbox(from=1,to=8,increment=1)
 hello = tk.Label(text="¿Cuantas zonas tiene tu casa?")
 hello.pack()
 button = tk.Button(text="Pulsa")
