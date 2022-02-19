9f324e7bb66faa617ec3912d87bf1b6c1bec6efe
Sbastdia <91331243+Sbastdia@users.noreply.github.com>
2022-02-19 17:39:47 +0000

    actualizaciones del proyecto

--- main.py
+++ main.py
@@ -1,17 +1,65 @@
-
-import tkinter as tk
-import alimpiar
-import lista
-import diccionarios
+import tkinter
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
 import script
-import tupla
-window1 = tk.Tk()
-window1.title("Hora de hacer una Roomba")
-window1.geometry("300x300")
-zonas=tk.Spinbox(from=1,to=8,increment=1)
-hello = tk.Label(text="¿Cuantas zonas tiene tu casa?")
-hello.pack()
-button = tk.Button(text="Pulsa")
-button.pack()
 
-tk.mainloop()
+class Pestana():
+    def __init__(self, nb):
+        self.pl = Frame(nb)
+        self.lblancho = Label(self.pl, text="Ancho")
+        self.spinancho = Spinbox(self.pl, from_=1, to=5)
+        self.lbllargo = Label(self.pl, text="Largo")
+        self.spinlargo = Spinbox(self.pl, from_=1, to=5)
+        self.lblancho.grid(column=0, row=0)
+        self.spinancho.grid(column=1, row=0)
+        self.lbllargo.grid(column=0, row=1)
+        self.spinlargo.grid(column=1, row=1)
+
+window = Tk()
+window.title("Hagamos una Roomba")
+window.geometry("400x255")
+
+pestadicionales = []
+
+nb = ttk.Notebook(window)
+nb.pack(fill="both", expand="yes")
+color="light steel blue"
+pl = Frame(nb, background=color)
+nb.add(pl, text="Zonas")
+
+
+window.configure(background=color)
+
+lbl = Label(pl, text="¿Cuantas zonas tiene el lugar?",bg=color,fg="black")
+lbl.grid(column=6, row=0)
+
+spin = Spinbox(pl, from_=1, to=20)
+spin.grid(column=6, row=1)
+
+
+def calcula():
+    superficie=0
+    for pest in pestadicionales:
+        ancho=int(pest.spinancho.get())
+        largo=int(pest.spinlargo.get())
+        superficie+=(ancho*largo)
+    messagebox.showinfo(message=str(superficie) + " metros cuadrados", title="Superficie total")
+
+btn2 = Button(window, text="Calcula", command=calcula)
+
+def creasalas():
+    numerosalas = int(spin.get())
+    for i in range(numerosalas):
+        pestaux = Pestana(nb)
+        nombre="Sala " + str(i+1)
+        nb.add(pestaux.pl, text=nombre)
+        pestadicionales.append(pestaux)
+    pl.destroy()
+    btn2.pack()
+
+btn = Button(pl, text="Pulsa", command=creasalas)
+btn.grid(column=6, row=3)
+
+
+window.mainloop()
